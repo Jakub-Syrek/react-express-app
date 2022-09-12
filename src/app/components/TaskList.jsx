c580f08466bacc8875661ca406220d21bb3c2de2
Jakub Syrek <jakub.syrek@microsoft.wsei.edu.pl>
2022-09-12 22:58:53 +0200

    task detail working, buttons added

--- src/app/components/TaskList.jsx
+++ src/app/components/TaskList.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { connect } from 'react-redux';
+import {Link} from "react-router-dom";
+import {requestTaskCreation} from '../store/mutations';
 
-export const TaskList = ({tasks, name})=>(
+export const TaskList = ({tasks, name, id, createNewTask})=>(
     <div>
         <h3>
             {name}
         </h3>
         <div>
 
-            {tasks.map(task=>(<div>{task.name}</div>))}
+            {tasks.map(task=>(
+                <Link to={`/task/${task.id}`} key={task.id}>
+                    <div>{task.name}</div>
+                </Link>
+                ))}
         </div>
+        <button onClick={()=>createNewTask(id)}>Add new</button>
     </div>
 )
 
@@ -21,4 +28,12 @@ const mapStateToProps = (state, ownProps)=>{
     }
 };
 
-export const ConnectedTaskList = connect (mapStateToProps) (TaskList);
+const mapDispatchToProps = (dispatch, ownProps)=>{
+    return {
+        createNewTask(id){
+            console.log("Creating new task", id);
+        }
+    }
+}
+
+export const ConnectedTaskList = connect (mapStateToProps, mapDispatchToProps) (TaskList);
