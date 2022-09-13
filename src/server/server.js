db196bef4c8d43be894646a7f78e26d575bfbe15
Jakub Syrek <jakub.syrek@microsoft.wsei.edu.pl>
2022-09-13 20:54:39 +0200

    express server adding new tasks

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import bodyparser from 'body-parser';
+import {connectDB} from './connect-db'
 
 let port = 7777;
 
@@ -8,6 +9,41 @@ let app = express();
 
 app.listen(port,console.log("Server listening on port", port));
 
-app.get('/',(req,res)=>{
-    res.send("Hello body world");
-})
+// app.get('/',(req,res)=>{
+//     res.send("Hello body world");
+// })
+
+app.use(
+    cors(),
+    bodyparser.urlencoded({extended:true}),
+    bodyparser.json()
+);
+
+export const addNewTask = async task=>{
+    let db = await connectDB();
+    let collection = db.collection(`tasks`);
+    await collection.insertOne(task)
+}
+
+export const updateTask = async task =>{
+    let {id, group, isComplete, name } = task;
+    let db = await connectDB();
+    let collection = db.collection(`tasks`);
+
+    if(group){
+        await collection.updateOne({id},{$set:{group}})
+    }
+    if(name){
+        await collection.updateOne({id},{$set:{name}})
+    }
+    if(isComplete !== undefined){
+        await collection.updateOne({id},{$set:{isComplete}})
+    }
+
+}
+
+app.post('/task/update', async(req,res)=>{
+   let task = req.body.task;
+   await updateTask(task);
+   res.status(200).send();
+});
