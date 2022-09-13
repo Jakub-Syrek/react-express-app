3796fb9a8cbf7ec23f6c05e530a4feb4d8db9ef6
Jakub Syrek <jakub.syrek@microsoft.wsei.edu.pl>
2022-09-13 08:31:03 +0200

    Store expanded with reducers

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { defaultState } from "../../server/defaultState";
 import { createLogger } from "redux-logger";
 import createSagaMiddleware from 'redux-saga';
@@ -6,11 +6,28 @@ import createSagaMiddleware from 'redux-saga';
 const sagaMiddleWare = createSagaMiddleware();
 import * as sagas from './sagas.mock'
 import {taskCreationSaga} from "./sagas.mock";
+import * as mutations from './mutations'
+
 
 export const store = createStore(
-    function reducer(state = defaultState, action){
-        return state;
-    },
+    combineReducers({
+      tasks(tasks = defaultState.tasks, action){
+          switch (action.type){
+              case mutations.CREATE_TASK:
+                  console.log(action);
+          }
+          return tasks;
+      },
+        comments(comments = defaultState.comments){
+          return comments;
+        },
+        groups(groups = defaultState.groups){
+            return groups;
+        },
+        users(users = defaultState.users){
+            return users;
+        }
+    }),
     applyMiddleware(createLogger(), sagaMiddleWare)
 );
 
